3b9e120f0496adac4068078be763efbcc8c04e4b
Michal Semla <michal.semla@nokia.com>
2021-08-13 17:43:54 +0300

    Use occurrences counter from responses
    * Makes request to get number of occurrences from service
    * Updates button text after button click

--- extension/src/api.js
+++ extension/src/api.js
@@ -9,3 +9,13 @@ export const postTab = url => fetch(`${serviceUrl}/tabs`, {
     },
     body: JSON.stringify({url})
 });
+
+export const countOccurrences = url => fetch(`${serviceUrl}/tabs/count`, {
+    method: 'POST',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({url})
+});
--- extension/src/index.js
+++ extension/src/index.js
@@ -1,5 +1,4 @@
-const {get, set} = require('./urlOccurencesStorage');
-const {postTab} = require('./api');
+const {postTab, countOccurrences} = require('./api');
 
 const div = document.createElement('cloud-tab-container');
 div.classList.add('cloudtab-container');
@@ -8,22 +7,19 @@ const button = document.createElement('cloud-tab-button');
 button.classList.add('cloudtab-button');
 const url = document.location.href;
 
-get(url).then(value => {
-    const numberOfSavedOccurrences = value >= 0 ? value : 0;
-    button.innerHTML = `Save (${numberOfSavedOccurrences})`;
-});
-
-div.addEventListener('click', () => {
-    postTab(url).then(() => {
-        get(url).then(value => {
-            const updatedNumberOfSaves = value >= 0 ? value + 1 : 1;
-            set(url, updatedNumberOfSaves).then(() => {
-                button.innerHTML = `Save (${updatedNumberOfSaves})`;
-            });
-        });
-        console.log('sent')
+countOccurrences(url)
+    .then(response => response.json())
+    .then(({occurrences}) => {
+        button.innerHTML = `Save (${occurrences})`;
     });
 
+div.addEventListener('click', () => {
+    postTab(url)
+        .then(response => response.json())
+        .then(({occurrences}) => {
+            button.innerHTML = `Save (${occurrences})`;
+            console.log('sent')
+        });
 });
 
 div.appendChild(button);
