f987f6c4f1bdc1fa2b27267b44159bd03f61f937
Michal Semla <michal.semla@nokia.com>
2021-08-13 17:42:10 +0300

    Count occurrences of saved urls
    * Added new endpoint to count occurrences for url
    * Response body of postTabs contains number of occurrences

--- server/routes/tabs.js
+++ server/routes/tabs.js
@@ -18,15 +18,30 @@ router.get('/', (req, res, next) => {
     res.render('tabs', {title: "CloudTab service", tabs});
 });
 
+function countOccurrences(urlToSave) {
+    return tabs.filter(({url}) => url === urlToSave).length;
+}
+
 router.post('/', (req, res, next) => {
+    let urlToSave = req.body.url;
     const newTab = {
         id: uuidv4(),
-        url: req.body.url,
+        url: urlToSave,
         time: Date.now()
     };
     tabs.push(newTab);
 
-    res.sendStatus(201);
+    res.status(201).send({
+        occurrences: countOccurrences(urlToSave)
+    });
+});
+
+router.post('/count', (req, res, next) => {
+    let urlToSave = req.body.url;
+
+    res.status(200).send({
+        occurrences: countOccurrences(urlToSave)
+    });
 });
 
 router.post('/delete/:id', (req, res, next) => {
